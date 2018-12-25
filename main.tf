47eeeb0233ac141eff6509b76a2ec47940b79622
jrbeverly <jrbeverly@live.com>
2018-12-25 18:50:36 -0500

    Copy in the terraform certificate module used internally

---
+++ main.tf
@@ -0,0 +1,23 @@
+resource "aws_acm_certificate" "default" {
+  domain_name               = "${var.domain_name}"
+  subject_alternative_names = ["${var.subject_alternative_names}"]
+  validation_method         = "DNS"
+}
+
+resource "aws_route53_record" "validation" {
+  count   = "${length(var.subject_alternative_names) + 1}"
+  zone_id = "${var.hosted_zone_id}"
+  ttl     = "${var.validation_record_ttl}"
+
+  name    = "${lookup(aws_acm_certificate.default.domain_validation_options[count.index], "resource_record_name")}"
+  type    = "${lookup(aws_acm_certificate.default.domain_validation_options[count.index], "resource_record_type")}"
+  records = ["${lookup(aws_acm_certificate.default.domain_validation_options[count.index], "resource_record_value")}"]
+}
+
+resource "aws_acm_certificate_validation" "default" {
+  certificate_arn = "${aws_acm_certificate.default.arn}"
+
+  validation_record_fqdns = [
+    "${aws_route53_record.validation.*.fqdn}",
+  ]
+}
