321ed1a8ebbdbbe9bf0cb210d5a12551c1f6344e
jrbeverly <jrbeverly@live.com>
2019-03-07 20:05:16 -0500

    Revise the naming of the variables, add docs

--- variables.tf
+++ variables.tf
@@ -1,11 +1,21 @@
-variable "domain_name" {}
-
-variable "subject_alternative_names" {
-  type = "list"
+variable "domain_name" {
+  type        = "string"
+  description = "A domain name for which the certificate should be issued"
 }
 
-variable "hosted_zone_id" {}
+variable "subject_alternative_names" {
+  type        = "list"
+  description = "A list of domains that should be SANs in the issued certificate"
+  default     = []
+}
 
-variable "validation_record_ttl" {
-  default = "60"
+variable "zone_id" {
+  type        = "string"
+  description = "The ID of the hosted zone to contain the validation record(s)."
+}
+
+variable "ttl" {
+  type        = "string"
+  description = "The TTL of the validation record(s)."
+  default     = "60"
 }
